c9af1006998e4b4d1ad971baab0c11948c2da71c
Josef Heinen <j.heinen@fz-juelich.de>
2021-03-23 14:00:16 +0100

    GR: fix build script

--- G/GR/build_tarballs.jl
+++ G/GR/build_tarballs.jl
@@ -3,49 +3,63 @@
 using BinaryBuilder
 
 name = "GR"
-version = v"0.52.0"
+version = v"0.56.1"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/sciapp/gr.git", "90ee73c741271d964f241f098b0dd3ba18eae271"),
+    GitSource("https://github.com/sciapp/gr.git", "c5fbc6613de1437a3dd598680de02af8dbe1098f"),
+    FileSource("https://github.com/sciapp/gr/releases/download/v$version/gr-$version.js",
+               "1e372730b864b105a4091cb2fd0814156aea8992ef0db308b9695956a292acc8", "gr.js")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
+cd $WORKSPACE/srcdir/gr
+
 if test -f "$prefix/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake"; then
     sed -i 's/_qt5gui_find_extra_libs.*AGL.framework.*//' $prefix/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
 fi
 
-if [[ $target == *"mingw"* ]]; then
-    winflags=-DCMAKE_C_FLAGS="-D_WIN32_WINNT=0x0f00"
-fi
+update_configure_scripts
+
+make -C 3rdparty/qhull -j${nproc}
 
 mkdir build
 cd build
-cmake $winflags -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_FIND_ROOT_PATH=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ../gr*
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_FIND_ROOT_PATH=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DGR_USE_BUNDLED_LIBRARIES=ON -DTIFF_LIBRARY=${libdir}/libtiff.${dlext} -DCMAKE_BUILD_TYPE=Release ..
+
 VERBOSE=ON cmake --build . --config Release --target install -- -j${nproc}
-install_license $WORKSPACE/srcdir/gr*/LICENSE.md
+cp ../../gr.js ${libdir}/
+
+install_license $WORKSPACE/srcdir/gr/LICENSE.md
 
 if [[ $target == *"apple-darwin"* ]]; then
-    cd $prefix/lib
-    ln -s libGR.so libGR.dylib
-    ln -s libGR3.so libGR3.dylib
-    ln -s libGRM.so libGRM.dylib
-    ln -s libGKS.so libGKS.dylib
-    cd ../bin
+    cd $prefix/bin
     ln -s ../Applications/gksqt.app/Contents/MacOS/gksqt ./
+    ln -s ../Applications/GKSTerm.app/Contents/MacOS/GKSTerm ./
 fi
 """
+
+#platforms = supported_platforms()
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Linux(:i686, libc=:glibc),
     Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Linux(:powerpc64le, libc=:glibc),
+#    Linux(:i686, libc=:musl),
+#    Linux(:x86_64, libc=:musl),
+#    Linux(:aarch64, libc=:musl),
+#    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+    MacOS(:x86_64),
+    FreeBSD(:x86_64),
+    Windows(:i686),
     Windows(:x86_64)
 ]
 platforms = expand_cxxstring_abis(platforms)
-push!(platforms, MacOS(:x86_64))
 
 # The products that we will ensure are always built
 products = [
@@ -65,9 +79,8 @@ dependencies = [
     Dependency("GLFW_jll"),
     Dependency("JpegTurbo_jll"),
     Dependency("libpng_jll"),
-    Dependency("Libtiff_jll"),
     Dependency("Pixman_jll"),
-    Dependency("Qhull_jll"),
+    Dependency("Libtiff_jll"),
     Dependency("Qt_jll"),
     BuildDependency("Xorg_libX11_jll"),
     BuildDependency("Xorg_xproto_jll"),
@@ -76,4 +89,5 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 # GCC version 7 because of ffmpeg, but building against Qt requires v8 on Windows.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"8")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               preferred_gcc_version = v"8")
